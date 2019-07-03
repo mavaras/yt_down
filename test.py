3db944b8a1e9886038776d6d2ac517c32efca1df
Mario Vrs <elasgard@hotmail.es>
2019-07-03 15:04:22 +0200

    heroku

--- test.py
+++ test.py
@@ -2,10 +2,14 @@ import sys
 import os
 from pathlib import Path, PureWindowsPath
 from pytube import YouTube
+import moviepy.editor as mp
 
 from flask import Flask, current_app, render_template, request, send_file, redirect, url_for
 app = Flask(__name__)
-
+def page_not_found_404(e): return render_template("error.html"), 404
+def page_not_found_500(e): return render_template("error.html"), 500
+app.register_error_handler(404, page_not_found_404)
+app.register_error_handler(500, page_not_found_500)
 
 @app.route("/", methods = ["GET"])
 def g_index():
@@ -13,17 +17,20 @@ def g_index():
 
 @app.route("/", methods = ["POST"])
 def p_index():
-    try:
-        link = request.form["link"]
-        video = YouTube(link)
-        ext = "mp4" if "mp4" in request.form or len(request.form) == 1 else "mp3"
-        title, length = video.title, video.length
+    print(request.form)
     
-        video.streams.filter(file_extension=ext).first().download("/tmp", filename="download")  # os.getcwd()+"\\temp_downloads"
-        
-        print("success")
-    except:
-        pass
+    link = request.form["link"]
+    video = YouTube(link)
+    ext = "mp4" if "mp4" in request.form or len(request.form) == 1 else "mp3"
+    title, length = video.title, video.length
+
+    video.streams.filter(file_extension="mp4").first().download("/tmp", filename="download")  # os.getcwd()+"\\temp_downloads"
+
+    if ext == "mp3":
+        clip = mp.VideoFileClip("/tmp/download.mp4")
+        clip.audio.write_audiofile("/tmp/download.mp3")
+
+    print("success")
     
     return render_template("index.html", link=link,
                                          title=title,
@@ -44,12 +51,6 @@ def get_download(ext):
     res = current_app.response_class(generate(), mimetype="video/"+ext)
     res.headers.set("Content-Disposition", "attachment", filename="download."+ext)
     return res'''
-
-@app.route("/download", methods = ["GET", "POST"])
-def download():
-    if request.method == "POST":
-        print(request.form["algo"])
-        return render_template("download.html", algo = request.form["algo"])
 
 
 if __name__ == "__main__":
